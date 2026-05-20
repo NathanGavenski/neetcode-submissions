0b7202029fd458913ba76da5e35716e0031fe021
Nathan <nathangavenski@gmail.com>
2026-05-20 16:06:49 +0100

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        st_anagram = defaultdict(int)
+        for x in s:
+            st_anagram[x] += 1
+
+        nd_anagram = defaultdict(int)
+        for x in t:
+            nd_anagram[x] += 1
+
+        return st_anagram == nd_anagram
