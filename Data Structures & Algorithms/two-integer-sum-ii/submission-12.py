dc01b80b360ea78270e1c5a1b348422457090624
Nathan <nathangavenski@gmail.com>
2026-05-21 21:30:17 +0100

    Add: two-integer-sum-ii - submission-12

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-12.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l_pivot = 0
+        r_pivot = len(numbers) - 1
+        while l_pivot < r_pivot:
+            current = numbers[l_pivot] + numbers[r_pivot]
+
+            if current == target:
+                return [l_pivot + 1, r_pivot + 1]
+            if current > target:
+                r_pivot -= 1
+                continue
+            if current < target:
+                l_pivot += 1
+                continue
